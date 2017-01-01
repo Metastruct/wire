a3bfd61e6ba8834e4730aa906363ea956355cb4e
Python1320 <python1320@users.noreply.github.com>
2017-01-01 17:19:43 +0200

    Revert "Merge branch 'ropt'"
    This reverts commit 2aa02db76a9959856044f30670ee5c94715fb452, reversing
    changes made to 91769955ec77542ce7429f670aa9ad452de33088.

--- lua/weapons/gmod_tool/stools/wire_adv.lua
+++ lua/weapons/gmod_tool/stools/wire_adv.lua
@@ -307,7 +307,7 @@ elseif CLIENT then
 	TOOL.ShowEntity = false -- bool for showing "Create Entity" output
 
 	function TOOL:Holster()
-		if IsValid(self.CurrentEntity) then self.CurrentEntity:SetNWString("BlinkWire", "") end
+		if IsValid(self.CurrentEntity) then self.CurrentEntity:SetNetworkedBeamString("BlinkWire", "") end
 		self.CurrentEntity = nil
 		self.Wiring = {}
 		self.WiringRender = {}
@@ -625,7 +625,7 @@ elseif CLIENT then
 			end
 
 			if oldport ~= self.CurrentWireIndex then
-				ent:SetNWString("BlinkWire", check[self.CurrentWireIndex][1])
+				ent:SetNetworkedBeamString("BlinkWire", check[self.CurrentWireIndex][1])
 				self:GetOwner():EmitSound("weapons/pistol/pistol_empty.wav")
 			end
 			return true
@@ -697,7 +697,7 @@ elseif CLIENT then
 
 			-- Clear blinking wire
 			if IsValid( self.AimingEnt ) then
-				self.AimingEnt:SetNWString("BlinkWire", "")
+				self.AimingEnt:SetNetworkedBeamString("BlinkWire", "")
 			end
 
 			if IsValid( ent ) then
@@ -718,7 +718,7 @@ elseif CLIENT then
 
 					-- Set blinking wire
 					if check[self.CurrentWireIndex] then
-						ent:SetNWString("BlinkWire", check[self.CurrentWireIndex][1])
+						ent:SetNetworkedBeamString("BlinkWire", check[self.CurrentWireIndex][1])
 					end
 				end
 			end
