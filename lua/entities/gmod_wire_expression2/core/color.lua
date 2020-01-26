023e9cfd59868ba2ce4c0ba5dd08b153d44322b1
Python1320 <python1320+gh@gmail.com>
2020-01-26 18:33:27 +0200

    Optimize hsv2rgb
    From https://gist.github.com/GigsD4X/8513963

--- lua/entities/gmod_wire_expression2/core/color.lua
+++ lua/entities/gmod_wire_expression2/core/color.lua
@@ -105,15 +105,54 @@ end
 
 --- HSV
 
+
+local math=math
+local function HSVToRGB( hue, saturation, value )
+	-- Returns the RGB equivalent of the given HSV-defined color
+	-- (adapted from some code found around the web)
+
+	-- If it's achromatic, just return the value, clamp yourself
+	if saturation == 0 then
+		return value,value,value
+	end
+	
+	hue=hue%360
+	
+	-- Get the hue sector
+	local hue_sector = math.floor( hue / 60 )
+	local hue_sector_offset = ( hue / 60 ) - hue_sector
+
+	local p = value * ( 1 - saturation )
+	local q = value * ( 1 - saturation * hue_sector_offset )
+	local t = value * ( 1 - saturation * ( 1 - hue_sector_offset ) )
+
+	if hue_sector == 0 then
+		return value, t, p
+	elseif hue_sector == 1 then
+		return q, value, p
+	elseif hue_sector == 2 then
+		return p, value, t
+	elseif hue_sector == 3 then
+		return p, q, value
+	elseif hue_sector == 4 then
+		return t, p, value
+	elseif hue_sector == 5 then
+		return value, p, q
+	end
+end
+
+local function HSVToRGB(H,S,V)
+	local r,g,b = HSVToRGB(H,S,V)
+	return r*255,g*255,b*255
+end
+
 --- Converts <hsv> from the [http://en.wikipedia.org/wiki/HSV_color_space HSV color space] to the [http://en.wikipedia.org/wiki/RGB_color_space RGB color space]
 e2function vector hsv2rgb(vector hsv)
-	local col = HSVToColor(hsv[1], hsv[2], hsv[3])
-	return { col.r, col.g, col.b }
+	return { HSVToRGB(hsv[1], hsv[2], hsv[3]) }
 end
 
 e2function vector hsv2rgb(h, s, v)
-	local col = HSVToColor(h, s, v)
-	return { col.r, col.g, col.b }
+	return { HSVToRGB(h, s, v) }
 end
 
 --- Converts <rgb> from the [http://en.wikipedia.org/wiki/RGB_color_space RGB color space] to the [http://en.wikipedia.org/wiki/HSV_color_space HSV color space]
