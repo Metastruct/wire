d987502aa9587b42eab99695880f3e56b15ef881
Python1320 <python1320+gh@gmail.com>
2022-10-13 21:37:54 +0300

    Add applyTorqueCenter

--- lua/entities/gmod_wire_expression2/core/entity.lua
+++ lua/entities/gmod_wire_expression2/core/entity.lua
@@ -588,6 +588,16 @@ e2function void entity:applyOffsetForce(vector force, vector position)
 	phys:ApplyForceOffset(force, position)
 end
 
+e2function void entity:applyTorqueCenter(vector angularImpulse)
+	if not validPhysics(this) then return self:throw("Invalid physics object!", nil) end
+	if not isOwner(self, this) then return self:throw("You do not own this entity!", nil) end
+
+	angularImpulse = clamp(angularImpulse)
+
+	local phys = this:GetPhysicsObject()
+	phys:ApplyTorqueCenter(angularImpulse)
+end
+
 e2function void entity:applyAngForce(angle angForce)
 	if not validPhysics(this) then return self:throw("Invalid physics object!", nil) end
 	if not isOwner(self, this) then return self:throw("You do not own this entity!", nil) end
